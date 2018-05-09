5c86ecbfc0949a8eb45c02f2ada4501ea908005f
Jerald Freeman <jerald@analogrepublic.com>
2018-05-08 19:53:56 -0700

    Updated all Signal text to Bymeft

--- SignalMessaging/attachments/SignalAttachment.swift
+++ SignalMessaging/attachments/SignalAttachment.swift
@@ -313,7 +313,7 @@ public class SignalAttachment: NSObject {
         if let filename = sourceFilename {
             return filename.filterFilename()
         } else {
-            let kDefaultAttachmentName = "signal"
+            let kDefaultAttachmentName = "Bymeft"
 
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "YYYY-MM-dd-HHmmss"
